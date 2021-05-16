bd8d1e4878382e2867820a0ec45ea5e4e491a5bf
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2021-05-17 00:11:54 +0530

    Updated oracle and bet token

--- contracts/mock/Oracle.sol
+++ contracts/mock/Oracle.sol
@@ -2,41 +2,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.7.0;
 
-interface AggregatorV3Interface {
-    function decimals() external view returns (uint8);
-
-    function description() external view returns (string memory);
-
-    function version() external view returns (uint256);
-
-    // getRoundData and latestRoundData should both raise "No data present"
-    // if they do not have data to report, instead of returning unset values
-    // which could be misinterpreted as actual reported values.
-    function getRoundData(uint80 _roundId)
-        external
-        view
-        returns (
-            uint80 roundId,
-            int256 answer,
-            uint256 startedAt,
-            uint256 updatedAt,
-            uint80 answeredInRound
-        );
-
-    function latestRoundData()
-        external
-        view
-        returns (
-            uint80 roundId,
-            int256 answer,
-            uint256 startedAt,
-            uint256 updatedAt,
-            uint80 answeredInRound
-        );
-}
+import "../AggregatorV3Interface.sol";
 
 // File: contracts/PriceOracle.sol
-contract PriceOracle is AggregatorV3Interface {
+contract PriceOracle is IAggregatorV3Interface {
     uint8 public override decimals = 8;
     string public override description = "BTC/USD";
     uint256 public override version = 1;
