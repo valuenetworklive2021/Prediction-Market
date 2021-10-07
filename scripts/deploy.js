7054d6246bcbc27fce38f502f0a1927ddd833a43
Ameesha <ameeshaagrawal12@gmail.com>
2021-10-08 01:36:13 +0530

    Added needed scripts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,3 +14,12 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+/**
+ * TO VERIFY CONTRACT
+ 1. hardhat run --network networkName scripts/deploy.js
+
+ * Then, copy the deployment address and paste it in to replace `DEPLOYED_CONTRACT_ADDRESS` in this command:
+ 2. npx hardhat verify --network networkName DEPLOYED_CONTRACT_ADDRESS
+ 
+ */
